1646a5f22b04ef6d7c63030ae861524c17c03dec
ruymtnw <ryuya.matsunawa@seattleconsulting.co.jp>
2022-09-13 10:00:00 +0900

    feat: validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,4 @@
 class Book < ApplicationRecord
-    validates :title, presence:true
-    validates :body, presence:true, length: { maximum: 200 }
+    validates :title, presence: true
+    validates :body, presence: true, length: { maximum: 200 }
 end

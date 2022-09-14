42d5c86bb5b6f04ce34d7d6d7c5da353b7a8e3d2
ruymtnw <ryuya.matsunawa@seattleconsulting.co.jp>
2022-09-14 12:26:48 +0900

    feat: success

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,7 +11,7 @@ class BooksController < ApplicationController
     def create
         @book = Book.new(book_params)
         if @book.save
-            redirect_to book_path(@book.id)
+            redirect_to book_path(@book.id), notice: 'Book was successfully created.'
         else
             @books = Book.all
             render :index
@@ -25,7 +25,7 @@ class BooksController < ApplicationController
     def update
         @book = Book.find(params[:id])
         if @book.update(book_params)
-            redirect_to book_path(@book.id)
+            redirect_to book_path(@book.id), notice: 'Book was successfully updated.'
         else
             render :edit
         end
